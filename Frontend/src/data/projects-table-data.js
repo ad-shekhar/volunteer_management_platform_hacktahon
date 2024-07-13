17059d0fb215a2bc8cde4c3d70960c987f1634ab
Vaibhav-Kumar-K-R <132189791+Vaibhav-Kumar-K-R@users.noreply.github.com>
2024-07-14 05:03:23 +0530

    Add files via upload

---
+++ Frontend/src/data/projects-table-data.js
@@ -0,0 +1,27 @@
+export const projectsTableData = [
+    {
+      img: "/img/logo-xd.svg",
+      name: "Material XD Version",
+      members: [
+        { img: "/img/team-1.jpeg", name: "Romina Hadid" },
+        { img: "/img/team-2.jpeg", name: "Ryan Tompson" },
+        { img: "/img/team-3.jpeg", name: "Jessica Doe" },
+        { img: "/img/team-4.jpeg", name: "Alexander Smith" },
+      ],
+      budget: "$14,000",
+      completion: 60,
+    },
+    {
+      img: "/img/logo-atlassian.svg",
+      name: "Add Progress Track",
+      members: [
+        { img: "/img/team-2.jpeg", name: "Ryan Tompson" },
+        { img: "/img/team-4.jpeg", name: "Alexander Smith" },
+      ],
+      budget: "$3,000",
+      completion: 10,
+    },
+  
+  ];
+  
+  export default projectsTableData;
